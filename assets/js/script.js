dbae13db97da947cee18d0499e606360c259fade
Irvens R Dupuy <92906651+irvensd@users.noreply.github.com>
2021-12-31 11:59:54 -0500

    Weather dashboard parse response

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ function initPage() {
     let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
     // Assigning a unique API to a variable
-    const APIKey = "84b79da5e5d7c92085660485702f4ce8";
+    const APIKey = "caddaaf6120069938f96e370d15cf61b";
 
     function getWeather(cityName) {
         // Execute a current weather get request from open weather api
@@ -24,4 +24,17 @@ function initPage() {
 
                 todayweatherEl.classList.remove("d-none");
 
-//            
+                // Parse response to display current weather
+                const currentDate = new Date(response.data.dt * 1000);
+                const day = currentDate.getDate();
+                const month = currentDate.getMonth() + 1;
+                const year = currentDate.getFullYear();
+                nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+                let weatherPic = response.data.weather[0].icon;
+                currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+                currentPicEl.setAttribute("alt", response.data.weather[0].description);
+                currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+                currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+                currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+                
+                
